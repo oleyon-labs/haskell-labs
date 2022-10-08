adef043563e48a45f63950d3fbe8fbfe99c22bf3
oley <kruuula@mail.ru>
2022-10-08 23:58:05 +0300

    first commit

---
+++ HelloWorld.hs
@@ -0,0 +1,64 @@
+import Language.Haskell.TH (valD)
+--main = putStrLn "hello world"
+--func1 a b = a + b
+--func2 a b = if a > b then a else b
+discount price minPrice disc =
+    if price < minPrice
+        then price
+        else price - (price - minPrice) * disc / 100
+
+discEval price minPrice disc = price - (price - minPrice) * disc / 100
+
+discount' price minPrice disc = let
+    discVal = (price - minPrice) * disc / 100
+    discEval' price minPrice disc = price - (price - minPrice) * disc / 100
+    in if price < minPrice
+        then price
+        else price - discVal
+
+discount'' price minPrice disc = 
+    if price < minPrice
+        then price
+        else price - discVal
+    where
+        discVal = (price - minPrice) * disc / 100
+        discEval' price minPrice disc = price - (price - minPrice) * disc / 100
+
+--перевод 16 ричные цифры в 10 ричные
+--hexNum hex =
+--    if hex == 'a' then "10"
+--    else if hex == 'b' then "11"
+--    else "error"
+translate ch =
+    case ch of
+        'A' -> "10"
+        'B' -> "11"
+        'C' -> "12"
+        'D' -> "13"
+        'E' -> "14"
+        'F' -> "15"
+        otherwise -> "error"
+
+translate' 'A' = let {x = "10"; y = "11"} in x
+translate' 'B' = "11"
+translate' 'C' = "12"
+translate' 'D' = "13"
+translate' 'E' = "14"
+translate' 'F' = "15"
+translate' _ = "error"
+    where {x = "10"; y = "11"}
+
+translate'' val
+    | val == 'A' = let {x = "100"; y = "110"} in x
+    | val == 'B' = x
+    | val == 'C' = y
+    | val == 'D' = "13"
+    | otherwise = "error"
+    where {x = "10"; y = "11"}
+
+conditionalFunc bool val1 val2 = if bool then val1 else val2
+
+solveQuadraticEquation a b c =
+    if a == 0
+        then - c / b
+        else 2
---
+++ Lesson3.hs
@@ -0,0 +1,172 @@
+import System.Win32 (COORD(y, x), _open_osfhandle, nORM_IGNORECASE, BY_HANDLE_FILE_INFORMATION (bhfiCreationTime))
+id' x = (\ x -> x) x
+
+id'' x = (\ y -> x) 5
+
+id2' = (\ x -> x)
+
+div5 = ( / 5)
+
+sum' x y = x + y
+
+sum'' :: Int -> Int -> Int
+sum'' x y = x + y
+
+--div'' :: Num a => a -> a -> a
+--div'' x y = x / y
+
+div5' = (/) 5
+
+test x y z = x + y * z
+
+testX = test 5
+testXY = test 2 3
+
+testXZ = \ x -> test 2 x 5
+
+factorial x = if x > 1 then x * factorial (x - 1) else 1
+
+factorial' x
+    | x == 1 = 1
+    | otherwise = x * factorial' (x - 1)
+
+fac' x
+    | x == 1 = 1
+    | otherwise = helper x 1
+    where helper x acc
+            | x == 1 = acc
+            | otherwise = helper (x-1) (acc * x)
+
+integralCalc f a b n = h * ((f a + f b) / 2 + sum)
+    where
+        h = (b - a) / n
+        sum = helper (a + h) 0
+        helper x acc
+            | x >= b = acc
+            | otherwise = helper (x + h) (acc + f x)
+--integralCalc (\ x -> 3 * x ^ 2) 0 3 100
+
+fib n = helper 1 1 n
+    where
+        helper n1 n2 n
+            | n == 1 = n2
+            | otherwise = helper n2 (n1 + n2) (n - 1)
+
+fib' n
+    | n == 1 = 0
+    | n == 2 = 1
+    | otherwise = fib' (n - 2) + fib' (n - 1)
+
+
+fib'' 0 = 0
+fib'' 1 = 1
+fib'' 2 = 1
+fib'' n = helper 0 1 n
+    where
+        helper pp p n
+            | n == 0 = p
+            | otherwise = helper p (pp + p) (n - 1)
+
+fst' (x, _, _) = x
+snd' (_, x, _) = x
+trd' (_, _, x) = x
+
+
+a = [1, 2, 3]
+b = [1..10]
+c = [0, 2..10]
+d = [10, 9..1]
+e = [(-10)..(-1)]
+f = [0.1, 0.2..2.3] :: [Float]
+
+i = [1..]
+
+m = [x ^ y |
+   x <- [1 .. 10], even x, y <- [1 .. 10], y < 4, y > 1]
+
+maxxy = [max (x ^ 2) y |
+    x <- [(- 5) .. (- 1)], x <= (- 2), y <- [1 .. 5], y >= 2]
+
+-- x:xs
+head' (x:_) = x
+tail' (_:xs) = xs
+
+length' (x)
+    | null x = 0
+    | otherwise = helper x 1
+    where
+        helper (x:xs) n
+            | null xs = n
+            | otherwise = helper xs (n + 1)
+
+length'' [] = 0
+length'' (x:xs) = 1 + length'' xs
+
+length''' xs = foldr (\ x -> (+) 1) 0 xs
+
+max'' (x:xs) = helper xs x
+    where
+        helper (x:xs) mx
+            | null xs = mx
+            | otherwise = if x > mx then helper xs x else helper xs mx
+
+
+maximum' [x] = x
+maximum' (x:xs) = max x (maximum' xs)
+
+take' n x = helper n x []
+    where
+        helper n x s
+            | n <= 0 || null x = s
+            | otherwise = helper (n-1) (tail x) (s ++ [head x])
+
+take'' n x = helper n x []
+    where
+        helper n x s
+            | n <= 0 || null x = reverse s
+            | otherwise = helper (n-1) (tail x) (head x : s)
+--дореализовать take
+
+take''' :: Int -> [Int] -> [Int]
+take''' 0 _ = []
+take''' _ [] = []
+take''' n (x:xs)
+    | n < 0 = []
+    | otherwise = x : take''' (pred n) xs
+
+reverse' :: [a] -> [a]
+reverse' [] = []
+reverse' (x:xs) = reverse' xs ++ [x]
+
+elem' :: (Eq a) => a -> [a] -> Bool
+elem' el [] = False
+elem' el (x:xs) 
+    | el == x = True
+    | otherwise = elem' el xs
+
+lf = [(^2), (+1),succ]
+f' = (^2)
+
+reverse'' :: [a] -> [a]
+--reverse'' [] = []
+reverse'' s = helper s []
+    where
+        helper xs xsn
+            | null xs = xsn
+            | otherwise = helper (tail xs) (head xs:xsn)
+
+
+myFoldl :: (b -> a -> b) -> b -> [a] -> b
+myFoldl f ini [] = ini
+myFoldl f ini (x:xs) = myFoldl f (f ini x) xs
+
+myFoldr :: (a -> b -> b) -> b -> [a] -> b
+myFoldr f ini [] = ini
+myFoldr f ini (x:xs) = f x (myFoldr f ini xs)
+
+reverse''' :: [a] -> [a]
+reverse''' = foldl (\l x -> x : l) []
+
+-- f (g (x))   f . g $ x
+
+fun (a, b) = a + b
